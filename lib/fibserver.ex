b00d16909f54ab31ee9bf96b70bca4d403605be1
s_arme <s.arme@ya.ru>
2021-02-13 03:44:52 +0330

    Implement start_link and init functions

--- lib/fibserver.ex
+++ lib/fibserver.ex
@@ -1,4 +1,15 @@
 defmodule Fibserver do
   use GenServer
 
+  # client side
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [])
+  end
+  # server side /callback functions
+  def init(init) do
+    # Trap exits
+    Process.flag(:trap_exit, true)
+    {:ok, init}
+  end
+
 end
