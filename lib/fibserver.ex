00435c62debd31aef218ebeefe65155c409ec6e1
s_arme <s.arme@ya.ru>
2021-02-13 03:48:16 +0330

    Define Scheduler module which create new process of Fibserver for requests and displays the reuslts as it gets ready

--- lib/fibserver.ex
+++ lib/fibserver.ex
@@ -41,4 +41,43 @@ defmodule Fibserver do
     end
   end
 end
+
+defmodule Scheduler do
+  def run(num_processes, module, func, to_calculate) do
+    pids =
+      1..num_processes
+      |> Enum.map(fn _ ->
+        {:ok, pid} = Fibserver.start_link([])
+        pid
+      end)
+
+    chunks_to_calculate = chunker(to_calculate, num_processes)
+
+    Enum.zip(pids, chunks_to_calculate)
+    |> Enum.map(fn {pid, chunk_to_calculate} ->
+      spawn(module, func, [pid, chunk_to_calculate])
+    end)
+  end
+
+  def schedule_processes(pid, chunk_to_calculate) when length(chunk_to_calculate) > 0 do
+    Fibserver.start(pid)
+    [head | tail] = chunk_to_calculate
+    IO.inspect({head, Fibserver.get(pid, head)})
+    schedule_processes(pid, tail)
+  end
+
+  def schedule_processes(pid, _chunk_to_calculate) do
+    Fibserver.end_server(pid)
+  end
+
+  # utils
+  defp chunker(list, parts), do: do_chunk(list, parts, [])
+
+  defp do_chunk(_, 0, chunks), do: chunks
+
+  defp do_chunk(to_chunk, parts, chunks) do
+    chunk_length = to_chunk |> length() |> div(parts)
+    {chunk, rest} = Enum.split(to_chunk, chunk_length)
+    do_chunk(rest, parts - 1, [chunk | chunks])
+  end
 end
